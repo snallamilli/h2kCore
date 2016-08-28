fdd35116efcbcb5175f3dbd1593a8d7df0307130
snallamilli <malavika.nallamilli@gmail.com>
2016-08-27 19:55:31 -0500

    second commit

---
+++ src/h2kinfo/core/AssignmentThree.java
@@ -0,0 +1,16 @@
+package h2kinfo.core;
+
+import java.util.Scanner;
+
+public class AssignmentThree {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int i = 1;i<=10;i++){
+            System.out.println(N +" x "+i+" = " + (N*i));
+        }
+
+	}
+
+}
